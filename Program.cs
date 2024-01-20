4a2c1ae54f1908a82f4f646f0846f4ea6e6e75e5
haiderinam7 <157199010+haiderinam7@users.noreply.github.com>
2024-01-20 20:20:16 +1100

    Connect to sql database

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Data;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;User Id=sa;Password=SQLConnect1;";
+
+            IDbConnection dbConnection = new SqlConnection(connectionString);
+
+            string sqlCommand = "SELECT GETDATE()";
+
+            DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand);
+            Console.WriteLine(rightNow);
+        }
+    }
+}
--- obj/Debug/net8.0/DotNETCore8LabProject1.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotNETCore8LabProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNETCore8LabProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6686c7d6a170740c9e804be16f76b647538ce5a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNETCore8LabProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNETCore8LabProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
