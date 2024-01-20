64449c5e4ffb57d2793ab403bd20771bba33a65c
haiderinam7 <157199010+haiderinam7@users.noreply.github.com>
2024-01-20 21:22:21 +1100

    Use dapper to execute sql query

--- Program.cs
+++ Program.cs
@@ -1,22 +1,56 @@
 ﻿using System;
+using System.Collections;
 using System.Data;
 using Dapper;
 using Microsoft.Data.SqlClient;
 
 namespace MyApp
 {
-    internal class Program
+    internal class Computer
     {
+        public string Motherboard { get; private set; }
+        public bool HasWifi { get; private set; }
+        public bool HasLTE { get; private set; }
+        public DateTime ReleaseDate { get; private set; }
+        public decimal Price { get; private set; }
+        public string VideoCard { get; private set; }
+
         static void Main(string[] args)
         {
             string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;User Id=sa;Password=SQLConnect1;";
 
             IDbConnection dbConnection = new SqlConnection(connectionString);
 
-            string sqlCommand = "SELECT GETDATE()";
+            Computer myComputer = new Computer()
+            {
+                Motherboard = "Z690",
+                HasWifi = true,
+                HasLTE = false,
+                ReleaseDate = DateTime.Now,
+                Price = 943.87m,
+                VideoCard = "RTX 2060"
+            };
 
-            DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand);
-            Console.WriteLine(rightNow);
+            string sql = @"INSERT INTO TutorialAppSchema.Computer (
+                Motherboard,
+                HasWifi,
+                HasLTE,
+                ReleaseDate,
+                Price,
+                VideoCard
+            ) VALUES('" + myComputer.Motherboard
+                + "','" + myComputer.HasWifi
+                + "','" + myComputer.HasLTE
+                + "','" + myComputer.ReleaseDate.ToString("yyyy-MM-dd HH:mm:ss")
+                + "','" + myComputer.Price
+                + "','" + myComputer.VideoCard
+                + "')";
+
+            Console.WriteLine(sql);
+
+            int result = dbConnection.Execute(sql);
+
+            Console.WriteLine(result);
         }
     }
 }
